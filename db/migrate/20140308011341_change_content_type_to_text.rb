94edae86f8dbd3f4462f0a137d6c9033490eb707
yangbohr <yangbo@umeng.com>
2014-03-07 21:36:40 -0800

    edits homepage

---
+++ db/migrate/20140308011341_change_content_type_to_text.rb
@@ -0,0 +1,10 @@
+class ChangeContentTypeToText < ActiveRecord::Migration
+  def self.up 
+    change_column :blogs, :content, :text
+  end
+
+  def self.down 
+    change_column :blogs, :content, :string
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305072713) do
+ActiveRecord::Schema.define(version: 20140308011341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "author"
     t.datetime "created_at"
     t.datetime "updated_at"
