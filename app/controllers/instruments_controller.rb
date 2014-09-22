c2f3df2e95fa628bb70987abe01e7e5f076f887d
yangbohr <yangbo@umeng.com>
2014-09-22 00:59:13 -0700

    fix

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,6 +1,6 @@
 class InstrumentsController < ApplicationController
   before_action :set_instrument, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /instruments
   # GET /instruments.json
   def index
